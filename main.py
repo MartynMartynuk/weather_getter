85dbb4b4e4ca595024008a4126ae902735ae3395
Martyn <95570045+MartynMartynuk@users.noreply.github.com>
2022-01-09 19:10:34 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import requests
 
 places = ['Лондон', 'Шереметьево', 'Череповец']
-
-try:
-    for place in places:
-        url = "https://wttr.in/{}".format(place)
-        settings = {'nTq':'', 'lang':'ru'}
-        response = requests.get(url, params=settings)
-        response.raise_for_status()
-        author_application = -48    #строка с ссылкой на автора weather.in
-        print(response.text[:author_application])
-except requests.exceptions.ConnectionError:
-    print('ERROR! ConnectionError')
+for place in places:
+    url = "https://wttr.inaa/{}".format(place)
+    settings = {'nTq': '', 'lang': 'ru'}
+    response = requests.get(url, params=settings)
+    response.raise_for_status()
+    author_application = -48    #для удаления строки с ссылкой на автора weather.in
+    print(response.text[:author_application])
