3c5eda39d3d6264b80f0d6733bbafb9503fe4774
Martyn <95570045+MartynMartynuk@users.noreply.github.com>
2022-01-09 21:28:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -2,9 +2,11 @@ import requests
 
 places = ['Лондон', 'Шереметьево', 'Череповец']
 for place in places:
-    url = "https://wttr.inaa/{}".format(place)
+    url = "https://wttr.in/{}".format(place)
     settings = {'nTq': '', 'lang': 'ru'}
     response = requests.get(url, params=settings)
     response.raise_for_status()
-    author_application = -48    #для удаления строки с ссылкой на автора weather.in
+    # для удаления строки с ссылкой на автора weather.in
+    author_application = -48
     print(response.text[:author_application])
+    
